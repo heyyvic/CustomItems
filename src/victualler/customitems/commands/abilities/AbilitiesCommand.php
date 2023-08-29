283007b7e3befb82ecb2b0e79701b2815fed8237
VictuallerDev <victuallerdev@gmail.com>
2023-08-28 20:42:17 -0600

    Update AbilitiesCommand.php

--- src/victualler/customitems/commands/abilities/AbilitiesCommand.php
+++ src/victualler/customitems/commands/abilities/AbilitiesCommand.php
@@ -4,17 +4,29 @@ namespace victualler\customitems\commands\abilities;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+use victualler\customitems\item\Abilities;
+use victualler\customitems\item\presets\Strength;
 
 class AbilitiesCommand extends Command {
 
     public function __construct()
     {
-        parent::__construct("abilities", "", "", []);
+        parent::__construct("abilities");
+        $this->setPermissions(["abilities.command"]);
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args)
-    {
-
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+        if(!$this->testPermission($sender) || !($sender instanceof Player)) {
+            $sender->sendMessage(TextFormat::colorize("&cYou do not have permissions to use this command."));
+            return;
+        }
+        if($sender->getInventory()->canAddItem(new Strength())) {
+            $sender->getInventory()->addItem(new Strength());
+        } else {
+            $sender->dropItem(new Strength());
+        }
     }
 
 }
